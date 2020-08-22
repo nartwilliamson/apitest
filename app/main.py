94b58bb48d908e3c9732250df0072d023331873b
ndrdul <nathandrdul@gmail.com>
2020-08-22 17:13:00 -0400

    make it better

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 
-
+from tiingo import TiingoClient
+import numpy as np
+import pandas as pd
 
 
 app = Flask(__name__)
+
+
+config = {}
+
+# To reuse the same HTTP Session across API calls (and have better performance), include a session key.
+config['session'] = True
+
+# If you don't have your API key as an environment variable,
+# pass it in via a configuration dictionary.
+config['api_key'] = "a5e87637752491cca8c3a282688dbe81f7243561"
+
+#quandl uUfmVoEksfwWxMA4tQcd
+# Initialize
+client = TiingoClient(config)
+
+
+Today = pd.datetime.today().strftime("%Y/%m/%d") 
+tickerList = ['TSLA', 'QQQ']
 
 @app.route('/')
 def index():
