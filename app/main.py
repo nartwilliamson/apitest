273f48492ff79b165b57cebe1e4b1e9f79deabcf
ndrdul <nathandrdul@gmail.com>
2020-08-22 16:58:28 -0400

    make it better

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
-from flask import Flask, jsonify
 
-from tiingo import TiingoClient
-import numpy as np
-import pandas as pd
+
+
 
 
 app = Flask(__name__)
@@ -14,6 +12,3 @@ def index():
   return "<h1>Welcome to CodingX</h1>"
 
 
-@app.route('/DOW')
-def getDow():
-  return "<h1>Getting DOW</h1>"
